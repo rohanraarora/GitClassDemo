3c44ca2f7d3d80b045fdc5418fae5dbe003f5fe8
Rohan Arora <rohanraarora@gmail.com>
2018-06-22 12:01:38 +0530

    Add toast

--- app/src/main/java/in/codingninjas/envision/gitdemo/MainActivity.java
+++ app/src/main/java/in/codingninjas/envision/gitdemo/MainActivity.java
@@ -2,6 +2,7 @@ package in.codingninjas.envision.gitdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toast.makeText(this,"Show",Toast.LENGTH_LONG).show();
+
     }
 }
